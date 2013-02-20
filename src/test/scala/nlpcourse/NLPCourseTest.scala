183a0e7db371717178bf517b4a5f3d28f28813ad
Stefan Bunk <stefan.bunk@googlemail.com>
2013-02-20 21:34:16 +0100

    Added first quiz for Viterbi algorithm.

--- src/test/scala/nlpcourse/NLPCourseTest.scala
+++ src/test/scala/nlpcourse/NLPCourseTest.scala
@@ -3,7 +3,23 @@ package nlpcourse
 import org.scalatest.FunSuite
 
 class NLPCourseTest extends FunSuite {
-	test("Assert true.") {
-		assert(true)
+	def fixture = new ViterbiAlgorithm()
+	lazy val D = Tag("D")
+	lazy val N = Tag("N")
+	lazy val V = Tag("V")
+
+	test("First Viterbi quiz is correctly calculated.") {
+		val viterbi = fixture
+		viterbi.trainQ(D, Star, Star, 1)
+		viterbi.trainQ(N, Star, D, 1)
+		viterbi.trainQ(V, D, N, 1)
+		viterbi.trainQ(STOP, N, V, 1)
+		viterbi.trainE("the", D, 0.8)
+		viterbi.trainE("dog", D, 0.2)
+		viterbi.trainE("dog", N, 0.8)
+		viterbi.trainE("the", N, 0.2)
+		viterbi.trainE("barks", V)
+		
+		viterbi.pi(3, N, V) should equal (0.64)
 	}
 }
