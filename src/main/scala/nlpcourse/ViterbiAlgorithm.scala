81014b9bd33c0ada7477f3c6ac78c9be502ddf77
Stefan Bunk <stefan.bunk@googlemail.com>
2013-02-21 18:27:05 +0100

    Added datastructures for q and e function.

--- src/main/scala/nlpcourse/ViterbiAlgorithm.scala
+++ src/main/scala/nlpcourse/ViterbiAlgorithm.scala
@@ -1,11 +1,20 @@
 package nlpcourse
 
+import scala.collection.mutable.{Map, Set}
+import nlpcourse._
 
 class ViterbiAlgorithm {
+	val e = Map[(String, Tag), Double]().withDefaultValue(0)
+	val q = Map[(Tag, Tag, Tag), Double]().withDefaultValue(0)
+
+	val tags = Set[Tag]()
 	def trainE(word: String, tag: Tag, prob: Double) {
+		e(word, tag) = prob
 	}
 
 	def trainQ(qi: Tag, qi_1: Tag, qi_2: Tag, prob: Double) {
+		tags += (qi, qi_1, qi_2)
+		q(qi, qi_1, qi_2) = prob
 	}
 
 	def pi(k: Int, qi_1: Tag, qi: Tag): Double = {
