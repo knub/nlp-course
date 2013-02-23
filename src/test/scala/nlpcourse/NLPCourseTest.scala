498f64ae90de2c64c1205e4877d5ecef79a926bc
Stefan Bunk <stefan.bunk@googlemail.com>
2013-02-23 22:47:05 +0100

    Refactored tests, removed duplication.

--- src/test/scala/nlpcourse/NLPCourseTest.scala
+++ src/test/scala/nlpcourse/NLPCourseTest.scala
@@ -4,38 +4,38 @@ import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 
 class NLPCourseTest extends FunSuite with ShouldMatchers {
-	def fixture = new ViterbiAlgorithm()
+	def firstQuiz: ViterbiAlgorithm = {
+		val viterbi = new ViterbiAlgorithm()
+		viterbi.trainQ(D, Star, Star, 1)
+		viterbi.trainQ(N, Star, D, 1)
+		viterbi.trainQ(V, D, N, 1)
+		viterbi.trainQ(STOP, N, V, 1)
+		viterbi.trainE("the", D, 0.8)
+		viterbi.trainE("dog", D, 0.2)
+		viterbi.trainE("dog", N, 0.8)
+		viterbi.trainE("the", N, 0.2)
+		viterbi.trainE("barks", V, 1)
+		viterbi
+
+	}
 	lazy val D = Tag("D")
 	lazy val N = Tag("N")
 	lazy val V = Tag("V")
 
 	test("First Viterbi quiz is correctly calculated using brute force algorithm.") {
-		val viterbi = fixture
-		viterbi.trainQ(D, Star, Star, 1)
-		viterbi.trainQ(N, Star, D, 1)
-		viterbi.trainQ(V, D, N, 1)
-		viterbi.trainQ(STOP, N, V, 1)
-		viterbi.trainE("the", D, 0.8)
-		viterbi.trainE("dog", D, 0.2)
-		viterbi.trainE("dog", N, 0.8)
-		viterbi.trainE("the", N, 0.2)
-		viterbi.trainE("barks", V, 1)
-		
+		val viterbi = firstQuiz
 		viterbi.piBruteForce(List("the", "dog", "barks"), 3, N, V) should be (0.64 plusOrMinus 0.0000001)
 	}
 
 	test("First Viterbi quiz is correctly calculated using dynamic programming.") {
-		val viterbi = fixture
-		viterbi.trainQ(D, Star, Star, 1)
-		viterbi.trainQ(N, Star, D, 1)
-		viterbi.trainQ(V, D, N, 1)
-		viterbi.trainQ(STOP, N, V, 1)
-		viterbi.trainE("the", D, 0.8)
-		viterbi.trainE("dog", D, 0.2)
-		viterbi.trainE("dog", N, 0.8)
-		viterbi.trainE("the", N, 0.2)
-		viterbi.trainE("barks", V, 1)
-		
+		val viterbi = firstQuiz
 		viterbi.pi(List("the", "dog", "barks"), 3, N, V)._2 should be (0.64 plusOrMinus 0.0000001)
+	}
+
+	test("First Viterbi quiz calculates correct tagging.") {
+		//val viterbi = firstQuiz
+		
+		// viterbi.pi(List("the", "dog", "barks"), 3, N, V)._2 should be (0.64 plusOrMinus 0.0000001)
+
 	}
 }
