3872c383db117487952f1e74174091b6cae5496d
Stefan Bunk <stefan.bunk@googlemail.com>
2013-03-16 21:57:44 +0100

    Parse training set and replace rare words with _RARE_.

--- src/main/scala/nlpcourse/assignment_1/Assignment1Main.scala
+++ src/main/scala/nlpcourse/assignment_1/Assignment1Main.scala
@@ -1,38 +1,54 @@
 package nlpcourse
 
-import scala.collection.mutable.Set
+import scala.collection.mutable.{Set, Map}
 import scalax.io._
 
 object Assignment1 extends App {
 
+	def inputFileName = "gene.train"
+
+	lazy val wordCount = Map[String, Int]().withDefaultValue(0)
+
 	override def main(args: Array[String]): Unit = {
-		val inputFileName = "gene.train"
+		print("Determing words to be replaced")
+		val wordsToBeReplaced = determineWordsToBeReplaced
+		println(": Done.")
+		print("Build new training file")
+		buildNewTrainingFile(wordsToBeReplaced)
+		println(": Done.")
+	}
 
-		val trainingFile = Resource.fromFile("assignment_1/%s.counts".format(inputFileName))
-		val lm = new LanguageModel
-		val lines = trainingFile.lines()
+	def determineWordsToBeReplaced: List[String] = {
+		val countTrainingFile = Resource.fromFile("assignment_1/%s.counts".format(inputFileName))
+		val lines = countTrainingFile.lines()
 
-		val wordsToBeReplaced: List[String] = lines.toList.filter { line =>
+		lines.toList.filter { line =>
 			line.contains("WORDTAG")
-		}.flatMap { line =>
+		}.foreach { line =>
 			val lineData = line.split(" ")
 			val count = lineData(0).toInt
 			val word = lineData(3)
-			if (count < 5)
-				List(word)
-			else
-				List()
+			wordCount(word) += count
 		}
 
-		val newTrainingFile = Resource.fromFile("assignment_1/%s.new".format(inputFileName))
+		wordCount.filter { case (word, wordCount) => wordCount < 5 }.keys.toList
+	}
 
+	def buildNewTrainingFile(wordsToBeReplaced: List[String]) {
+		val trainingFile = Resource.fromFile("assignment_1/%s".format(inputFileName))
+		val newTrainingFile = Resource.fromFile("assignment_1/%s.new".format(inputFileName))
+		val lines = trainingFile.lines()
+
+		val sb = new StringBuilder
 		lines.foreach { line =>
 			val lineData = line.split(" ")
-			val s = if (wordsToBeReplaced.contains(lineData(3)))
-				"%s %s %s _RARE_%n".format(lineData(0), lineData(1), lineData(2))
+			val s = if (wordsToBeReplaced.contains(lineData(0)))
+				"_RARE_ %s%n".format(lineData.drop(1).mkString(" "))
 			else
 				"%s%n".format(line)
-			newTrainingFile.append(s)
+			sb.append(s)
 		}
+		newTrainingFile.append(sb.toString)
+
 	}
 }
